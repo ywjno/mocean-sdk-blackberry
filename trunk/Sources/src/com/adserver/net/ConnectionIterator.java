d10872317bf2d2ae8337fd88c7bb9fa032d7f67c
okoshkin@mojiva.com <okoshkin@mojiva.com@de912b7e-b88d-be43-d4cd-23eec13b075e>
2011-04-25 12:23:10 +0000

    Delete old version

--- trunk/Sources/src/com/adserver/net/ConnectionIterator.java
+++
@@ -1,7 +0,0 @@
-package com.adserver.net;
-
-import javax.microedition.io.HttpConnection;
-
-public interface ConnectionIterator {
-    public HttpConnection getNextConnection() throws NoMoreTransportsException;
-}
